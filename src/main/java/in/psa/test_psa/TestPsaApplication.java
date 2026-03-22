5d5641467d85dbf113658e79981353cc1919eecf
Rohansingh3 <rohansingh0543@gmail.com>
2026-03-22 21:39:35 +0530

    first commit

---
+++ src/main/java/in/psa/test_psa/TestPsaApplication.java
@@ -0,0 +1,14 @@
+package in.psa.test_psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPsaApplication {
+	int x=0;
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPsaApplication.class, args);
+	}
+
+}
